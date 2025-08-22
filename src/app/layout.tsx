b20b55ca1c7855da6441327cef8147c370439eab
Petrov Dmitri <dp@insomniacdesign.com>
2025-08-22 18:48:32 +0300

    Added speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from 'next';
+import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'Signature',
   description: 'My personal collection of all my professional life',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children} <SpeedInsights />
+      </body>
     </html>
-  )
+  );
 }
